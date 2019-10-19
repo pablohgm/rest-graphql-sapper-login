d6d8233d047d5347b144c812d4819fb1d475501f
pablogonzalez <pagonzalez@nabancard.com>
2019-10-19 00:19:26 -0600

    feat(project): remove commitizen, add tslint, add prettier, config test

--- REST-API/server.ts
+++ REST-API/server.ts
@@ -1,15 +1,17 @@
-import { resolve } from "path"
-import { config } from "dotenv"
+import { config } from 'dotenv'
 import express from 'express'
+import { resolve } from 'path'
 
-config({ path: resolve(__dirname, "../.env") })
+config({ path: resolve(__dirname, '.env') })
 const port = process.env.PORT
 const server: express.Application = express()
 
 server.get('/', (req, res) => {
-    res.send('Hello World !!')
+  res.send('Hello World !!')
 })
 
 server.listen(port, () => {
-    console.log(`Rest API - 🌎 is listening on port ${port}`)
+  console.log(`Rest API - 🌎 is listening on port ${port}`)
 })
+
+export default server
